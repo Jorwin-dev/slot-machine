e4256fdd0be8f35988f23ce98d38fb089511231d
jorwin <jorwin@Xubuntu.myguest.virtualbox.org>
2023-11-02 15:14:44 -0600

    Add user input

---
+++ project.js
@@ -0,0 +1,22 @@
+// Deposit some money
+
+// Determine the number of lines to bet on
+
+// Collect an amount to bet
+
+// Spin the slots
+
+// Check if the user won or lost
+
+// Give the user winnings or keep the bet
+
+// Play again?
+
+// Import user prompt
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit();
